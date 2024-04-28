15671a2d52783134e3d680ef0f071c311cfdb741
bhusalprabhat <98088129+bhusalprabhat@users.noreply.github.com>
2024-04-28 21:42:45 +0545

    Add files via upload

---
+++ Votingage.java
@@ -0,0 +1,21 @@
+package JAVA;
+ /**
+ *
+ */
+class prabhat {
+
+   public static void main(String[] args) {
+     int age=20;//initialization of value is 20
+     int votingage=17;//initialization of value is 18
+     if(age>=votingage){
+      System.out.print("You are eligible to vote");
+     }
+     else{
+      System.out.println("You are eligible to vote");
+     }
+   }
+ }
+ 
+
+    
+
